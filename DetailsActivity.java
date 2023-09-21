b204921854fc7e988f13957047f142a4703a0fce
tekmilesahin8 <119962313+tekmilesahin8@users.noreply.github.com>
2023-09-21 23:46:42 +0300

    Add files via upload

---
+++ DetailsActivity.java
@@ -0,0 +1,32 @@
+package com.tekmile.proje2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.tekmile.proje2.databinding.ActivityDetailsBinding;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private ActivityDetailsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityDetailsBinding.inflate(getLayoutInflater());
+        View view =binding.getRoot();
+        setContentView(view);
+
+        Intent intent=getIntent();
+
+
+        //Casting
+       Landmark selectedLandmark=(Landmark) intent.getSerializableExtra("landmark");
+
+        binding.nameText.setText(selectedLandmark.name);
+        binding.countryText.setText(selectedLandmark.country);
+        binding.imageView.setImageResource(selectedLandmark.image);
+    }
+}
---
+++ Landmark.java
@@ -0,0 +1,17 @@
+package com.tekmile.proje2;
+
+import java.io.Serializable;
+
+public class Landmark implements Serializable {
+
+    String name;
+    String country;
+    int image;
+
+    public Landmark(String name, String country, int image){
+        this.name=name;
+        this.country=country;
+        this.image=image;
+    }
+
+}
